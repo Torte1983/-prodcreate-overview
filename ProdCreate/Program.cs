764017338d9e302fe72f2efcb00b77da09649ba3
Thorsten Teriet <t.teriet@gmail.com>
2020-02-05 21:41:46 +0100

    Projekt angelegt

---
+++ ProdCreate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProdCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
